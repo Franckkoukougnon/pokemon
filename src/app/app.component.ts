fdc07eaad36c06df1b72a9d822a27416cee07ecb
franck <franckkoukougnon@yahoo.fr>
2023-05-04 20:12:32 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pokemon';
+  pokemonList = POKEMONS;
+
+  selectedPokemon(Pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le Pokemon ${Pokemon.name}`);
+  }
+
+  constructor() {}
+  ngOnInit(): void {
+    console.table(this.pokemonList);
+    this.selectedPokemon(this.pokemonList[0]);
+  }
 }
